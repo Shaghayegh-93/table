df18a496057e3b53ddf04e71501f6d642df58d06
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-23 14:10:36 +0100

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import TableTwo from "./components/TableTwo";
-// import Table from "./components/Table";
+import Table from "./components/Table";
 
 function App() {
   return (
     <>
-      {/* <Table /> */}
-      <TableTwo/>
+      <Table />
     </>
   );
 }
